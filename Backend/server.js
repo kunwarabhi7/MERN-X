83235e313631644476120038cacb21e796dd6425
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-08-20 22:24:44 +0530

    initial commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(5000,()=>{
+    console.log("Backend server is running")
+})
