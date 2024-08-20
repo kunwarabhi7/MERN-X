5ddaeb82ab3c740f39d59dc30f2b396d531bc82e
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-08-20 23:31:33 +0530

    added db

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup =async (req, res) => {
+    res.json({
+       message: 'Signup complete'})
+ }
+
+ export const login =async (req, res) => {
+    res.json({
+       message: 'Signup complete'})
+ }
+
+ export const logout =async (req, res) => {
+    res.json({
+       message: 'Signup complete'})
+ }
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,27 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import cors from 'cors';
+import { authRouter } from './routes/auth.routes.js';
+import mongoDbConnect from './db/connectMongodb.js';
 
+const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(5000,()=>{
-    console.log("Backend server is running")
+app.use('/api/auth',authRouter);
+
+app.listen(PORT,()=>{
+    console.log(`Backend server is running ${PORT}`)
+    mongoDbConnect();
 })
