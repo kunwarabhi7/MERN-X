80b2a05ef5b5faeda2ab73bd0dcd8d25c68f01c7
Abhishek Parmar <abhishekparmar162@gmail.com>
2024-08-31 21:46:00 +0530

    added Get user route

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { ProtectRoute } from "../middleware/ProtectedRoute.js";
-import { getUserProfile } from "../controllers/user.controller.js";
+import { followUnfollowUser, getUserProfile } from "../controllers/user.controller.js";
 
 const router = new Router();
 
 router.get("/profile/:username",ProtectRoute, getUserProfile)
+router.post("/profile/",ProtectRoute,followUnfollowUser);
 
 export  {router as userRouter}
